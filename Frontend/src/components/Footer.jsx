4ee59fa1e12d05ba3cd1094d79be77700213a7cf
RHRONY <muhammadrony147@gmail.com>
2025-07-26 21:45:49 +0600

    footer was updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © 2024 CUET Connect. All rights reserved. Made with ❤️ for CUET students.
+            © 2025 CUET Connect. All rights reserved. Made with ❤️ for CUET students.
           </p>
         </div>
       </div>
